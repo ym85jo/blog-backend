0517941abf6f2582b2d8ec64c0714b61d4ec4bca
unknown <ehli@nate.com>
2020-01-27 00:50:26 +0900

    test

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -8,7 +8,7 @@ export const register = async ctx => {
     const schema = Joi.object().keys({
         username : Joi.string().alphanum().min(3).max(20).required()
         , password : Joi.string().required()
-    })
+    });
 
     const result = Joi.validate(ctx.request.body, schema);
     if(result.error){
